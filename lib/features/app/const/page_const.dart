b3e8591355fc39378a53dbd54aca37e7710102d9
rishitha.menusha <rishitha.menush@gmail.com>
2024-08-29 11:41:37 +0530

    add the page constant

---
+++ lib/features/app/const/page_const.dart
@@ -0,0 +1,27 @@
+class PageConst{
+
+  // * Auth Const
+  static const String welcomPage ="/";
+  static const String otpPage = "otpPage";
+  static const String loginPage = "loginPage";
+  static const String initialProfileSubmitPage = "initialProfileSubmitPage";
+
+  // * Home
+  static const String homePage = "homePage";
+
+  // * Contact Users Page
+  static const String contactUserPage = "contactUserPage";
+  static const String callContactsPage = "callContactsPage";
+
+  // * Settings
+  static const String settingsPage = "settingsPage";
+
+  // * Chat Page
+  static const String singleChatPage = "singleChatPage";
+
+  // * My Status Page
+  static const String myStatusPage = "myStatusPage";
+
+  // * Call Page
+  static const String callPage = "callPage";
+}
