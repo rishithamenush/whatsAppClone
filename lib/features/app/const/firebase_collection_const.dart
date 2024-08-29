890f7b35cbc76309ad1aa73ccc608a588add7c33
rishitha.menusha <rishitha.menush@gmail.com>
2024-08-29 11:04:55 +0530

    add the firebase collection const

---
+++ lib/features/app/const/firebase_collection_const.dart
@@ -0,0 +1,16 @@
+class FirebaseCollectionConst{
+  //user
+  static const String users = "users";
+
+  //Chat
+  static const String oneToOneChatChannel = "oneToOneChatChannel";
+  static const String chatChannel = "chatChannel";
+  static const String message = "messages";
+
+  //Status
+  static const String status = "status";
+
+  //Call
+  static const String call = "call";
+  static const String callHistory = "callHistory";
+}
