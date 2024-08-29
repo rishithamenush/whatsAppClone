13f71ba014b090e3ca61041d352cb514d62c29ef
rishitha.menusha <rishitha.menush@gmail.com>
2024-08-29 14:34:54 +0530

    Update the splash screen

--- lib/features/app/splash/splash_screen.dart
+++ lib/features/app/splash/splash_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/features/app/theme/style.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -25,9 +26,32 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      backgroundColor: backgroundColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(),
+          Image.asset("assets/whats_app_log.png", color: Colors.white, width: 100,height: 100,),
+          Column(
+            children: [
+              Text("from", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400, color: greyColor.withOpacity(.6)),),
+              const SizedBox(height: 10,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset("assets/meta.png", color: Colors.white, width: 35, height: 32,),
+                  const SizedBox(width: 5,),
+                  const Text("Meta", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),)
+                ],
+              ),
+              const SizedBox(height: 30,)
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
