c658103706377664fd49797014ef0b909698c409
rishitha.menusha <rishitha.menush@gmail.com>
2024-08-30 10:26:48 +0530

    Add the navigation of the next page

--- lib/features/user/presentation/pages/otp_page.dart
+++ lib/features/user/presentation/pages/otp_page.dart
@@ -61,7 +61,7 @@ class _OtpPageState extends State<OtpPage> {
             ),
             GestureDetector(
               onTap: (){
-
+                Navigator.push(context, MaterialPageRoute(builder: (context) => InitialProfileSubmitPage()));
               },
               child: Container(
                 margin: const EdgeInsets.only(bottom: 20),
