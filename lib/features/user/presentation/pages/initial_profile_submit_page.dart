b33d9ae3308af86577ff0bec23854c43761facce
rishitha.menusha <rishitha.menush@gmail.com>
2024-08-30 13:20:50 +0530

    Update the profile submit form

--- lib/features/user/presentation/pages/initial_profile_submit_page.dart
+++ lib/features/user/presentation/pages/initial_profile_submit_page.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:whatsapp_clone/features/app/const/app_const.dart';
+import 'package:whatsapp_clone/features/app/theme/style.dart';
+
+import '../widgets/profile_widget.dart';
 
 class InitialProfileSubmitPage extends StatefulWidget {
   const InitialProfileSubmitPage({super.key});
@@ -10,30 +16,78 @@ class InitialProfileSubmitPage extends StatefulWidget {
 }
 
 class _InitialProfileSubmitPageState extends State<InitialProfileSubmitPage> {
+
+  final TextEditingController _usernameController = TextEditingController();
+  File? _image;
+
+  Future<void> selectImage() async {
+    try {
+      final pickedFile = await ImagePicker.platform.getImage(source: ImageSource.gallery);
+
+      setState(() {
+        if (pickedFile != null) {
+          _image = File(pickedFile.path);
+        } else {
+          print("No image has been selected");
+        }
+      });
+    } catch (e) {
+      toast("Some error occurred: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: Container(
         margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 30),
         child: Column(
           children: [
-            const SizedBox(height: 30,),
+            const SizedBox(height: 30),
             Center(
-              child: Text("Profile Info", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+              child: Text(
+                "Profile Info",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
             ),
-            SizedBox(
-              height: 30,
-            ),
+            const SizedBox(height: 30),
             GestureDetector(
               onTap: selectImage,
               child: SizedBox(
-                width: 50,
-                height: 50,
+                width: 100,
+                height: 100,
                 child: ClipRRect(
-                  borderRadius: BorderRadius.circular(25),
+                  borderRadius: BorderRadius.circular(50),
                   child: profileWidget(image: _image),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10,),
+            Container(
+              height: 40,
+              margin: const EdgeInsets.only(top: 1.5),
+              decoration: const BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(color: tabColor, width: 1.5)
                 )
               ),
+              child: TextField(
+                controller: _usernameController,
+                decoration: const InputDecoration(
+                  hintText: "Username",
+                  border: InputBorder.none
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            GestureDetector(
+              onTap: (){},
+              child: Container(
+
+              ),
+
             )
           ],
         ),
--- lib/features/user/presentation/widgets/profile_widget.dart
+++ lib/features/user/presentation/widgets/profile_widget.dart
@@ -1,33 +1,33 @@
 import 'dart:io';
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/features/app/theme/style.dart';
 
-Widget profileWidget({String? imageUrl, File? image}){
-  if(image == null){
-    if(imageUrl == null || imageUrl == ""){
+Widget profileWidget({String? imageUrl, File? image}) {
+  if (image == null) {
+    if (imageUrl == null || imageUrl.isEmpty) {
       return Image.asset(
         'assets/profile_default.png',
         fit: BoxFit.cover,
       );
-    }else{
+    } else {
       return CachedNetworkImage(
         imageUrl: imageUrl,
         fit: BoxFit.cover,
-        progressIndicatorBuilder: (context , url , downloadProgress){
-          return const CircularProgressIndicator(color: tabColor,);
+        progressIndicatorBuilder: (context, url, downloadProgress) {
+          return const CircularProgressIndicator(color: tabColor);
         },
         errorWidget: (context, url, error) => Image.asset(
           'assets/profile_default.png',
-              fit: BoxFit.cover,
+          fit: BoxFit.cover,
         ),
       );
     }
+  } else {
+    return Image.file(
+      image,
+      fit: BoxFit.cover,
+    );
   }
-  else{
-    return Image.file(image,fit:BoxFit.cover);
-  }
-
 }
