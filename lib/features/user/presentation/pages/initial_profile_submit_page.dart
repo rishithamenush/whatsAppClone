edac0c82bb4eac8ff1fb8f52fde3ca6dbbaec285
rishitha.menusha <rishitha.menush@gmail.com>
2024-08-30 10:53:12 +0530

    Add the initial profile submit page

--- lib/features/user/presentation/pages/initial_profile_submit_page.dart
+++ lib/features/user/presentation/pages/initial_profile_submit_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class InitialProfileSubmitPage extends StatefulWidget {
   const InitialProfileSubmitPage({super.key});
@@ -10,6 +12,32 @@ class InitialProfileSubmitPage extends StatefulWidget {
 class _InitialProfileSubmitPageState extends State<InitialProfileSubmitPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 30),
+        child: Column(
+          children: [
+            const SizedBox(height: 30,),
+            Center(
+              child: Text("Profile Info", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            GestureDetector(
+              onTap: selectImage,
+              child: SizedBox(
+                width: 50,
+                height: 50,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(25),
+                  child: profileWidget(image: _image),
+                )
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
