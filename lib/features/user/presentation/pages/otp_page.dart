7df9c299086b160b7230af2df3630176f0224d79
rishitha.menusha <rishitha.menush@gmail.com>
2024-08-30 09:55:19 +0530

    Update the otp page UI changes

--- lib/features/user/presentation/pages/otp_page.dart
+++ lib/features/user/presentation/pages/otp_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/features/app/theme/style.dart';
 
 class OtpPage extends StatefulWidget {
   const OtpPage({super.key});
@@ -10,6 +12,43 @@ class OtpPage extends StatefulWidget {
 class _OtpPageState extends State<OtpPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 40),
+        child: const Column(
+          children: [
+            SizedBox(
+              height: 40,
+            ),
+            Center(
+              child: Text(
+                "Verify your OTP",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: tabColor
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Enter your OTP for the WhatsApp Clone Verification",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 15),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            _pinCodeWidget(),
+            SizedBox(
+              height: 30,
+            )
+
+          ],
+        ),
+      ),
+    );
   }
 }
