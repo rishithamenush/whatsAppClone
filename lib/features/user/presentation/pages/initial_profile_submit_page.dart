152bd32f14ccbe26fa4b6d497375a0f383d23b67
rishitha.menusha <rishitha.menush@gmail.com>
2024-09-02 14:03:16 +0530

    Add the Next Page

--- lib/features/user/presentation/pages/initial_profile_submit_page.dart
+++ lib/features/user/presentation/pages/initial_profile_submit_page.dart
@@ -43,64 +43,85 @@ class _InitialProfileSubmitPageState extends State<InitialProfileSubmitPage> {
         margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 30),
         child: Column(
           children: [
-            const SizedBox(height: 30),
-            const Center(
-              child: Text(
-                "Profile Info",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: tabColor
-              ),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              "Please Provide your name and an optional profile \nphoto",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 15),
-            ),
-            const SizedBox(height: 30),
-            GestureDetector(
-              onTap: selectImage,
-              child: SizedBox(
-                width: 100,
-                height: 100,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(50),
-                  child: profileWidget(image: _image),
+            Column(
+              children: [
+                const SizedBox(height: 30),
+                const Center(
+                  child: Text(
+                    "Profile Info",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: tabColor
+                  ),
+                  ),
                 ),
-              ),
-            ),
-            const SizedBox(height: 10,),
-            Container(
-              height: 40,
-              margin: const EdgeInsets.only(top: 1.5),
-              decoration: const BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(color: tabColor, width: 1.5)
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "Please Provide your name and an optional profile \nphoto",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 15),
+                ),
+                const SizedBox(height: 30),
+                GestureDetector(
+                  onTap: selectImage,
+                  child: SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: profileWidget(image: _image),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10,),
+                Container(
+                  height: 40,
+                  margin: const EdgeInsets.only(top: 1.5),
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(color: tabColor, width: 1.5)
+                    )
+                  ),
+                  child: TextField(
+                    controller: _usernameController,
+                    decoration: const InputDecoration(
+                      hintText: "Username",
+                      border: InputBorder.none
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                GestureDetector(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => const InitialProfileSubmitPage()));
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.only(bottom: 20),
+                    width: 120,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: tabColor,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "Next",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500
+                        ),
+                      ),
+                    ),
+                  ),
                 )
-              ),
-              child: TextField(
-                controller: _usernameController,
-                decoration: const InputDecoration(
-                  hintText: "Username",
-                  border: InputBorder.none
-                ),
-              ),
+              ],
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            GestureDetector(
-              onTap: (){},
-              child: Container(
-
-              ),
-
-            )
           ],
         ),
       ),
