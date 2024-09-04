1a2054894bb224e4b4e24670ad3d32f840ff1fa7
rishitha.menusha <rishitha.menush@gmail.com>
2024-09-04 17:04:03 +0530

    Update the Contact Page

--- lib/features/app/home/contacts_page.dart
+++ lib/features/app/home/contacts_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/features/user/presentation/widgets/profile_widget.dart';
 
 class ContactsPage extends StatefulWidget {
   const ContactsPage({super.key});
@@ -12,8 +13,22 @@ class _ContactsPageState extends State<ContactsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Select Contacts"),
+      ),
       body: ListView.builder(itemCount:20, itemBuilder: (context, index){
-        return const ListTile();
+        return  ListTile(
+          leading: SizedBox(
+            width: 50,
+            height: 50,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(25),
+              child: profileWidget(),
+            ),
+          ),
+          title: const Text("Username"),
+          subtitle: const Text("Hey there! I'm using WhatsApp"),
+        );
       })
     );
   }
