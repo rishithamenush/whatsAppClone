4862c6422f9a4f540ec9fc580541f36634aeacd2
rishitha.menusha <rishitha.menush@gmail.com>
2024-09-04 16:19:53 +0530

    Add the profile widget in StatusPage

--- lib/features/status/presentation/pages/status_page.dart
+++ lib/features/status/presentation/pages/status_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/features/user/presentation/widgets/profile_widget.dart';
 
 class StatusPage extends StatefulWidget {
   const StatusPage({super.key});
@@ -11,8 +12,31 @@ class StatusPage extends StatefulWidget {
 class _StatusPageState extends State<StatusPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Status Page"),),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      margin:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                      width: 60,
+                      height: 60,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(30),
+                        child: profileWidget(),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
