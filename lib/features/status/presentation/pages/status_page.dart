3f5263e371d89bbbd126475b4e2ad519bf4f92c5
rishitha.menusha <rishitha.menush@gmail.com>
2024-09-04 10:46:22 +0530

    Add the Status Page

---
+++ lib/features/status/presentation/pages/status_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class StatusPage extends StatefulWidget {
+  const StatusPage({super.key});
+
+  @override
+  State<StatusPage> createState() => _StatusPageState();
+}
+
+class _StatusPageState extends State<StatusPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("Status Page"),),
+    );
+  }
+}
