fd48697e17d85e47e7318ed1be36f9e3385e6a89
rishitha.menusha <rishitha.menush@gmail.com>
2024-09-05 10:56:20 +0530

    Add the my status page

---
+++ lib/features/status/presentation/pages/my_status_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class MyStatusPage extends StatefulWidget {
+  const MyStatusPage({super.key});
+
+  @override
+  State<MyStatusPage> createState() => _MyStatusPageState();
+}
+
+class _MyStatusPageState extends State<MyStatusPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
