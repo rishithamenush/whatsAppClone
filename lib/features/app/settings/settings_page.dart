3c80292914e8e656afa0a7217bf134d7ad707e00
rishitha.menusha <rishitha.menush@gmail.com>
2024-09-05 10:27:05 +0530

    Add the app bar for the settings page

--- lib/features/app/settings/settings_page.dart
+++ lib/features/app/settings/settings_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/features/user/presentation/widgets/profile_widget.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -10,6 +12,29 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Settings"),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              SizedBox(
+                width: 65,
+                height: 65,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(32.5),
+                  child:
+                  profileWidget(),
+                ),
+              ),
+
+              const
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
