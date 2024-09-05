e658c43ad92415a9fefcbb13380c5cd986086fb8
rishitha.menusha <rishitha.menush@gmail.com>
2024-09-05 11:19:56 +0530

    add the time_ago package

--- lib/features/status/presentation/pages/my_status_page.dart
+++ lib/features/status/presentation/pages/my_status_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/features/user/presentation/widgets/profile_widget.dart';
+import 'package:timeago/timeago.dart' as time_ago;
+
 
 class MyStatusPage extends StatefulWidget {
   const MyStatusPage({super.key});
@@ -14,6 +17,35 @@ class _MyStatusPageState extends State<MyStatusPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("My Status"),
+      ),
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 12.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  width: 55,
+                  height: 55,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(30),
+                    child: profileWidget(),
+                  ),
+                ),
+                const SizedBox(width: 15,),
+                Expanded(
+                  child: Text(
+                    time_ago.format(DateTime.now().subtract(Duration(seconds: DateTime.now().second))),
+                    style:  const TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
