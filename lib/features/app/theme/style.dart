b19ec0ef5d2c73ae11a874d75cafad3dd64940b4
rishitha.menusha <rishitha.menush@gmail.com>
2024-09-05 13:31:47 +0530

    add the message seen function

--- lib/features/app/theme/style.dart
+++ lib/features/app/theme/style.dart
@@ -13,4 +13,5 @@ const dividerColor = Color.fromRGBO(37, 45, 50, 1);
 const chatBarMessage = Color.fromRGBO(30, 36, 40, 1);
 const mobileChatBoxColor = Color.fromRGBO(31, 44, 52, 1);
 const greyColor = Colors.grey;
+const lightGreyColor = Color.fromRGBO(211, 211, 211, 1);
 const blackColor = Colors.black;
