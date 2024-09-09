b42bf4bf4963196c66fd384b2b21778c48d92b4b
rishitha.menusha <rishitha.menush@gmail.com>
2024-09-09 12:13:04 +0530

    Add the user data

--- lib/features/user/domain/entities/user_entity.dart
+++ lib/features/user/domain/entities/user_entity.dart
@@ -1,6 +1,13 @@
 import 'package:equatable/equatable.dart';
 
 class UserEntity extends Equatable{
+  final String? username;
+  final String? email;
+  final String? phoneNumber;
+  final bool? isOnline;
+  final String? uid;
+  final String? status;
+  final String? profileUrl;
   @override
   List<Object?> get props => [];
 }
