b99e0fe2e7c5d049cb063173b318887c8713f331
rishitha.menusha <rishitha.menush@gmail.com>
2024-09-11 12:34:25 +0530

    add the firebase authentication

--- lib/features/user/data/data_sources/remote/user_remote_data_source_impl.dart
+++ lib/features/user/data/data_sources/remote/user_remote_data_source_impl.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:whatsapp_clone/features/app/const/firebase_collection_const.dart';
 import 'package:whatsapp_clone/features/user/data/data_sources/remote/user_remote_data_source.dart';
+import 'package:whatsapp_clone/features/user/data/models/user_model.dart';
 import 'package:whatsapp_clone/features/user/domain/entities/contact_entity.dart';
 import 'package:whatsapp_clone/features/user/domain/entities/user_entity.dart';
 
@@ -16,8 +18,32 @@ class UserRemoteDataSourceImpl implements UserRemoteDataSource{
 
   @override
   Future<void> createUser(UserEntity user) {
-    // TODO: implement createUser
-    throw UnimplementedError();
+    final userCollection =
+    fireStore.collection(FirebaseCollectionConst.users);
+
+    final uid = await getCurrentUID();
+
+    final newUser = UserModel(
+      email: user.email,
+      uid: uid,
+      isOnline: user.isOnline,
+      phoneNumber: user.phoneNumber,
+      username: user.username,
+      profileUrl: user.profileUrl,
+      status: user.status
+    ).toDocument();
+
+    try{
+      userCollection.doc(uid).get().then((userDoc){
+        if(!userDoc.exists){
+          userCollection.doc(uid).set(newUser);
+        }else{
+          userCollection.doc(uid).update(newUser);
+        }
+      });
+    }catch(e){
+      throw Exception("Error occur while creating user");
+    }
   }
 
   @override
