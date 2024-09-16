458129c2721108d6bcd24e3cdb3a9c40ec4809e5
rishitha.menusha <rishitha.menush@gmail.com>
2024-09-16 21:17:11 +0530

    Update the User state

--- lib/features/user/presentation/cubit/user/user_cubit.dart
+++ lib/features/user/presentation/cubit/user/user_cubit.dart
@@ -1,6 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../../domain/entities/user_entity.dart';
+
 part 'user_state.dart';
 
 class UserCubit extends Cubit<UserState> {
--- lib/features/user/presentation/cubit/user/user_state.dart
+++ lib/features/user/presentation/cubit/user/user_state.dart
@@ -8,3 +8,25 @@ final class UserInitial extends UserState {
   @override
   List<Object> get props => [];
 }
+
+class UserLoading extends UserState{
+  @override
+
+  List<Object?> get props => [];
+
+}
+
+class UserLoaded extends UserState{
+  final List<UserEntity> users;
+
+  const UserLoaded({required this.users});
+  @override
+  List<Object?> get props => [users];
+
+}
+
+class UserFailure extends UserState{
+  @override
+  List<Object?> get props => [];
+
+}
