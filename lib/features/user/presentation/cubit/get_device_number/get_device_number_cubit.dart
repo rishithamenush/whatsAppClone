325e9f4007cd730fa0589f538aebbaed1c705cdd
rishitha.menusha <rishitha.menush@gmail.com>
2024-09-16 13:02:30 +0530

    Get device number cubit

--- lib/features/user/presentation/cubit/get_device_number/get_device_number_cubit.dart
+++ lib/features/user/presentation/cubit/get_device_number/get_device_number_cubit.dart
@@ -2,9 +2,22 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
 import '../../../domain/entities/contact_entity.dart';
+import '../../../domain/usecases/user/get_device_number_usecase.dart';
 
 part 'get_device_number_state.dart';
 
 class GetDeviceNumberCubit extends Cubit<GetDeviceNumberState> {
-  GetDeviceNumberCubit() : super(GetDeviceNumberInitial());
+  GetDeviceNumberUsecase getDeviceNumberUsecase;
+
+  GetDeviceNumberCubit({required this.getDeviceNumberUsecase})
+      : super(GetDeviceNumberInitial());
+
+  Future<void> getDeviceNumber() async{
+    try{
+      final contactNumbers = await getDeviceNumberUsecase.call();
+      emit(GetDeviceNumberLoaded(contacts: contactNumbers));
+    }catch(_){
+      emit(GetDeviceNumberFailure());
+    }
+  }
 }
