029017a672bae5f0b4b8c1a0694dcde233f789ae
rishitha.menusha <rishitha.menush@gmail.com>
2024-09-16 12:24:39 +0530

    Update the Credential states

--- lib/features/user/presentation/cubit/credential/credential_state.dart
+++ lib/features/user/presentation/cubit/credential/credential_state.dart
@@ -8,3 +8,30 @@ final class CredentialInitial extends CredentialState {
   @override
   List<Object> get props => [];
 }
+
+class CredentialSuccess extends CredentialState{
+  @override
+  List<Object?> get props => [];
+}
+
+class CredentialLoading extends CredentialState{
+  @override
+  List<Object?> get props => [];
+
+}
+
+class CredentialPhoneAuthSmsCodeReceived extends CredentialState{
+  @override
+  List<Object?> get props => [];
+}
+
+class CredentialPhoneAuthProfileInfo extends CredentialState{
+  @override
+  List<Object?> get props => [];
+}
+
+class CredentialFailure extends CredentialState{
+  @override
+  List<Object?> get props => [];
+
+}
