d79bfd9d080c38e1116449ca2e9c0220a25a372b
rishitha.menusha <rishitha.menush@gmail.com>
2024-09-16 15:01:31 +0530

    Update the get single user cubit

--- lib/features/user/presentation/cubit/get_single_user/get_single_user_cubit.dart
+++ lib/features/user/presentation/cubit/get_single_user/get_single_user_cubit.dart
@@ -1,10 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
 import 'package:whatsapp_clone/features/user/domain/entities/user_entity.dart';
+
+import '../../../domain/usecases/user/get_single_user_usecase.dart';
 
 part 'get_single_user_state.dart';
 
 class GetSingleUserCubit extends Cubit<GetSingleUserState> {
-  GetSingleUserCubit() : super(GetSingleUserInitial());
+  final GetSingleUserUsecase getSingleUserUsecase;
+  GetSingleUserCubit({
+    required this.getSingleUserUsecase
+}): super(GetSingleUserInitial());
+
+  Future<void> getSingleUser({required String uid}) async{
+    emit(GetSingleUserLoading());
+    final StreamedResponse = getSingleUserUsecase.call(uid);
+     StreamedResponse.listen((users){
+      emit(GetSingleUserLoaded(singleUser: users.first));
+    });
+  }
 }
