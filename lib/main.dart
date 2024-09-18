d1e098c224d7b67e8f688e400cddadad524cd5fe
rishitha.menusha <rishitha.menush@gmail.com>
2024-09-18 11:38:13 +0530

    add the firebase init in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/features/app/theme/style.dart';
 import 'package:whatsapp_clone/routes/on_generate_routes.dart';
+import 'firebase_options.dart';
 import 'main_injection_container.dart' as di;
 import 'features/app/splash/splash_screen.dart';
 
 void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await di.init();
   runApp(const MyApp());
 }
