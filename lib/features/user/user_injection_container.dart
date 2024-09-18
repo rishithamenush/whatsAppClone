85e83637f4bfde5f81b541c8e3a1babe0a7ed1ed
rishitha.menusha <rishitha.menush@gmail.com>
2024-09-18 09:56:20 +0530

    update the other main injection cudbits

--- lib/features/user/user_injection_container.dart
+++ lib/features/user/user_injection_container.dart
@@ -1,6 +1,9 @@
 import 'dart:ffi';
 
 import 'package:whatsapp_clone/features/user/presentation/cubit/auth/auth_cubit.dart';
+import 'package:whatsapp_clone/features/user/presentation/cubit/credential/credential_cubit.dart';
+import 'package:whatsapp_clone/features/user/presentation/cubit/get_device_number/get_device_number_cubit.dart';
+import 'package:whatsapp_clone/features/user/presentation/cubit/user/user_cubit.dart';
 
 import '../../main_injection_container.dart';
 
@@ -11,7 +14,23 @@ Future<void> userInjectionContainer() async {
   sl.registerFactory<AuthCubit> (() => AuthCubit(
       getCurrentUidUseCase: sl.call(),
       isSignInUseCase: sl.call(),
-      signOutUseCase: sl.call()));
+      signOutUseCase: sl.call()
+  ));
+
+  sl.registerFactory<UserCubit>(() => UserCubit(
+      updateUserUseCase: sl.call(),
+      getAllUsersUseCase: sl.call(),
+  ));
+
+  sl.registerFactory<CredentialCubit>(() => CredentialCubit(
+      signInWithPhoneNumberUseCase: sl.call(),
+      verifyPhoneNumberUseCase: sl.call(),
+      createUserUseCase: sl.call(),
+  ));
+
+  sl.registerFactory<GetDeviceNumberCubit>(() => GetDeviceNumberCubit(
+      getDeviceNumberUsecase: sl.call(),
+  ));
 
 
   //* USE CASES INJECTION
--- lib/main_injection_container.dart
+++ lib/main_injection_container.dart
@@ -1,6 +1,8 @@
 
 import 'package:get_it/get_it.dart';
 
+import 'features/user/user_injection_container.dart';
+
 final sl = GetIt.instance;
 
 Future<void> init() async{
