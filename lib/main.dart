06e4968c4df4dbd566dcc327352a54795ceedd80
rishitha.menusha <rishitha.menush@gmail.com>
2024-09-18 10:24:47 +0530

    update the other main injection cudbits

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/features/app/theme/style.dart';
 import 'package:whatsapp_clone/routes/on_generate_routes.dart';
-
+import 'main_injection_container.dart' as di;
 import 'features/app/splash/splash_screen.dart';
 
-void main(){
+void main() async{
+  await di.init();
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
--- lib/main_injection_container.dart
+++ lib/main_injection_container.dart
@@ -1,4 +1,6 @@
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 
 import 'features/user/user_injection_container.dart';
@@ -6,5 +8,13 @@ import 'features/user/user_injection_container.dart';
 final sl = GetIt.instance;
 
 Future<void> init() async{
+
+  final auth = FirebaseAuth.instance;
+  final fireStore = FirebaseFirestore.instance;
+
+  sl.registerLazySingleton(() => auth);
+  sl.registerLazySingleton(() => fireStore);
+
+
   await userInjectionContainer();
 }
